84c7a2aac88b10a986ff3e5283b44ddff73fccca
Jacob Ford <jwfhmp@umkc.edu>
2023-12-04 20:06:01 -0600

    Updated .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args){
+        int G = 25;
+        if (args.length > 0) {
+            try {G = Integer.parseInt(args[0]);}
+            catch (NumberFormatException e) {System.err.println("Invalid integer");}
+        }
+    }
+
+    private static void testRunningTimes(int G){
         TestExecutionTimes.writeDijkstraTimesToCSV(25, 1000);
         TestExecutionTimes.writeDijkstraTimesToCSV(25,9000);
         TestExecutionTimes.writeDijkstraTimesToCSV(25,17000);
